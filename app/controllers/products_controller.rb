716c6e10b720b5659d3bd0c4bb82e972f85c7684
Chris Cardle <cdcardle@gmail.com>
2019-01-15 16:10:44 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,8 +18,8 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @products = Product.create(product_params)
+    render json: @products
   end
 
   def show
